5b4aee65447a94605b5bdca27e4c0de290b71f36
Prayer07 <ezenwaokechukwu3@gmail.com>
2026-01-22 00:24:29 +0100

    omo login

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -1,16 +1,3 @@
-// import { Navigate, Outlet } from "react-router-dom";
-// import { useAuth } from "../hooks/useAuth";
-
-// export default function ProtectedRoute() {
-//   const { loading, isAuthenticated } = useAuth();
-
-//   if (loading) return <p>Checking session...</p>;
-
-//   return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
-// }
-
-
-
 import { Navigate, Outlet } from "react-router-dom"
 import { useAuth } from "../context/AuthContext"
 
@@ -18,7 +5,7 @@ export default function ProtectedRoute(){
   const { user, loading } = useAuth()
 
   if (loading) return <p>Loading...</p>
-  if (!user) return <Navigate to="/" replace />
+  if (!user) return <Navigate to="/login" replace />
 
   return <Outlet />
 }
